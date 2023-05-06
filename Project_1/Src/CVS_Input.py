ccfa46436ccf34a1e0376305dbcd41abbbbcfe6a
felix.peng <pon0531@gmail.com>
2023-05-06 22:30:28 +0800

    Change data base name to data base table name

--- Project_1/Src/CVS_Input.py
+++ Project_1/Src/CVS_Input.py
@@ -21,8 +21,7 @@ database_name = "Population"
 basic_file_path = "../Data/opendata"
 data_base_pre_name = "data_population_"
 basic_file_paths = []
-data_base_names = []
-
+data_base_table_names = []
 
 dirPath = r'../Data'
 print(os.listdir(dirPath))
@@ -34,7 +33,7 @@ for file_name in Files:
         basic_file_paths.append(file_path_create)
 
         file_name = file_name.replace(".csv","")
-        data_base_names.append(file_name)
+        data_base_table_names.append(file_name)
 
 try:
     # 開啟資料庫連接
@@ -61,6 +60,7 @@ try:
     print("Create database :",database_name)
 
     # 創造test_db後，再次connection
+
     conn = MySQLdb.connect(host="localhost",    # 主機名稱
                             user="root",         # 帳號
                             password="boopann403", # 密碼
@@ -68,8 +68,9 @@ try:
                             port=3306)           # port
     cursor = conn.cursor()
 
-    for i in range(len(Files)-1): #多一個readme檔
+    for i in range(len(data_base_table_names)):
         start_time_ = time.time()
+
         sql = """CREATE TABLE IF NOT EXISTS {IMG} (statistic_yyy int(4),
                                                   district_code CHAR(20),
                                                   site_id TEXT(10),
@@ -77,23 +78,22 @@ try:
                                                   village TEXT(10),
                                                   sex CHAR(10),
                                                   birthplace TEXT(10),
-                                                  population int(10))""".format(IMG=data_base_names[i])
-        print("Input database table:",data_base_names[i])
+                                                  population int(10))""".format(IMG=data_base_table_names[i])
+        print("Input database table:",data_base_table_names[i])
         cursor.execute(sql)
 
         #把cvs匯入data base, 從第1行開始讀，往下讀nrows行
-        data = pd.read_csv(basic_file_paths[i], header=1,nrows=10, encoding="utf8")
-        
-        #data = pd.read_csv(basic_file_paths[i], header=1, encoding="utf8")
+        data = pd.read_csv(basic_file_paths[i], header=1,nrows=15, encoding="utf8")
+
         for j in range(len(data)):
             sql = """INSERT INTO {IMG} (statistic_yyy, district_code, site_id, site_id_sub, village, sex,birthplace,population)
-                                   VALUES (%s, %s, %s, %s, %s, %s,%s ,%s)""".format(IMG=data_base_names[i])
-
+                                   VALUES (%s, %s, %s, %s, %s, %s,%s ,%s)""".format(IMG=data_base_table_names[i])
             var = (data.iloc[j,0], data.iloc[j,1], data.iloc[j,2][:3], data.iloc[j,2][3:], data.iloc[j,3], data.iloc[j,4],data.iloc[j,5],data.iloc[j,6])
             cursor.execute(sql, var)
             conn.commit()
+
         end_time_ = time.time()
-        print("Time elapsed for input %s: %.2f seconds" %(data_base_names[i],(end_time_ - start_time_)))
+        print("Time elapsed for input %s: %.2f seconds" %(data_base_table_names[i],(end_time_ - start_time_)))
 except Exception as e :
     print("資料庫連接失敗", e)
     
